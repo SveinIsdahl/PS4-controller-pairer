c5e1a1e88deb7c2502b3564a093f736047b56f46
Paul Straw <paulstraw@paulstraw.com>
2023-03-28 18:24:30 -0700

    Update to work with DS4 controllers

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # About
 
-A tool for viewing and setting the bluetooth address
-a sixaxis controller is currently paired with.
+A tool for viewing and setting the Bluetooth address a DualShock 4 controller is currently paired with. Based on <https://github.com/user-none/sixaxispairer>, which is designed for Sixaxis controllers, with a couple quick and dirty hacks on top.
 
 
 # Dependencies
 
-HID API (http://www.signal11.us/oss/hidapi/)
+HID API (http://www.signal11.us/oss/hidapi/). macOS users (i.e. `brew install hidapi`), check the comment in main.c.
 
 
 # Supported Platforms
@@ -18,9 +17,11 @@ HID API (http://www.signal11.us/oss/hidapi/)
 
 # Building
 
-    $ mkdir build
-    $ cd build
-    $ cmake ..
-    $ make
-    $ ./bin/sixaxispairer
-    $ ./bin/sixaxispairer xx:xx:xx:xx:xx:xx
+``` bash
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+$ ./bin/ds4pairer
+$ ./bin/ds4pairer xx:xx:xx:xx:xx:xx
+```
--- main.c
+++ main.c
@@ -23,16 +23,24 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <hidapi/hidapi.h>
+// macOS users, swap/adjust these includes:
+// https://github.com/user-none/sixaxispairer/issues/5
+// pkg-config --cflags hidapi
+#include </opt/homebrew/Cellar/hidapi/0.13.1/include/hidapi/hidapi.h>
+// #include <hidapi/hidapi.h>
 
-#define VENDOR        0x054c
-#define PRODUCT       0x0268
+#define VENDOR 0x054c
+#define PRODUCT 0x05C4
 /* 0xf5   == (0x03f5 & ~(3 << 8))
  * 0x03f5 == (0xf5 | (3 << 8))
  * HIDAPI will automatically add (3 << 8 to the report id.
  * Other tools for setting the report id use hid libraries which
  * don't automatically do this. */
-#define MAC_REPORT_ID 0xf5
+// https://coral.googlesource.com/bluez-imx/+/refs/tags/5.50/plugins/sixaxis.c#175
+#define GET_MAC_REPORT_ID 0x12
+// https://eleccelerator.com/unpairing-a-dualshock-4-and-setting-a-new-bdaddr/
+// https://coral.googlesource.com/bluez-imx/+/refs/tags/5.50/plugins/sixaxis.c#224
+#define SET_MAC_REPORT_ID 0x13
 
 static unsigned char char_to_nible(char c)
 {
@@ -56,6 +64,7 @@ static int mac_to_bytes(const char *in, size_t in_len, unsigned char *out, size_
 
     memset(out, 0, out_len);
     p = in;
+    char temp[6];
     while (p+1 < in+in_len && i < out_len) {
         if (*p == ':') {
             p++;
@@ -66,10 +75,18 @@ static int mac_to_bytes(const char *in, size_t in_len, unsigned char *out, size_
             return 0;
         }
 
-        out[i] = (char_to_nible(*p) << 4) | (char_to_nible(*(p+1) & 0xff));
+        temp[i] = (char_to_nible(*p) << 4) | (char_to_nible(*(p+1) & 0xff));
         i++;
         p += 2;
     }
+
+    // FIXME: This is jank, but it's late. DS4 is little-endian
+    out[0] = temp[5];
+    out[1] = temp[4];
+    out[2] = temp[3];
+    out[3] = temp[2];
+    out[4] = temp[1];
+    out[5] = temp[0];
 
     if (p < in+in_len)
         return 0;
@@ -78,16 +95,19 @@ static int mac_to_bytes(const char *in, size_t in_len, unsigned char *out, size_
 
 static void pair_device(hid_device *dev, const char *mac, size_t mac_len)
 {
-    unsigned char buf[8];
+    unsigned char buf[23];
     int           ret;
 
     memset(buf, 0, sizeof(buf));
-    buf[0] = MAC_REPORT_ID;
+    buf[0] = SET_MAC_REPORT_ID;
     buf[1] = 0x0;
-    if ((mac_len != 12 && mac_len != 17) || !mac_to_bytes(mac, mac_len, buf+2, sizeof(buf)-2)) {
+    if ((mac_len != 12 && mac_len != 17) || !mac_to_bytes(mac, mac_len, buf+1, sizeof(buf)-2)) {
         printf("Invalid mac\n");
         return;
     }
+    // https://coral.googlesource.com/bluez-imx/+/refs/tags/5.50/plugins/sixaxis.c#224
+    memset(buf + 7, 0, 16);
+    // An actual key is set in https://eleccelerator.com/unpairing-a-dualshock-4-and-setting-a-new-bdaddr/, but that didn't seem to work for me.
 
     ret = hid_send_feature_report(dev, buf, sizeof(buf));
     if (ret == -1) {
@@ -97,19 +117,19 @@ static void pair_device(hid_device *dev, const char *mac, size_t mac_len)
 
 static void show_pairing(hid_device *dev)
 {
-    unsigned char buf[8];
+    unsigned char buf[16];
     int           ret;
 
     memset(buf, 0, sizeof(buf));
-    buf[0] = MAC_REPORT_ID;
+    buf[0] = GET_MAC_REPORT_ID;
     buf[1] = 0x0;
 
     ret = hid_get_feature_report(dev, buf, sizeof(buf));
-    if (ret < 8) {
+    if (ret < 7) {
         printf("Read error\n");
         return;
     }
-    printf("%02x:%02x:%02x:%02x:%02x:%02x\n", buf[2], buf[3], buf[4], buf[5], buf[6], buf[7]);
+    printf("%02x:%02x:%02x:%02x:%02x:%02x\n", buf[15], buf[14], buf[13], buf[12], buf[11], buf[10]);
 }
 
 int main(int argc, char **argv)
